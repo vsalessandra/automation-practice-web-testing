42da5a976201f9d6671ccf24eede0c3b8c318b9d
Alessandra <alessandravitoria799@gmail.com>
2026-08-23 21:25:48 -0300

    chore: set up Cypress with Cucumber preprocessor
    Add Cypress and @badeball/cypress-cucumber-preprocessor so .feature
    files under e2e/ can be run as BDD specs, plus ignore node_modules
    and Cypress run artifacts.

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  return config;
+}
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.automationpratice.com.br",
+    specPattern: "e2e/**/*.feature",
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    setupNodeEvents,
+  },
+});
